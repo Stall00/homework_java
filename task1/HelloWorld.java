1fe57709eb66618929afbba51c0e107ad7198746
Oleksandr <78863703+verhovcev214265@users.noreply.github.com>
2024-03-18 13:55:42 +0100

    Test

--- task1/HelloWorld.java
+++ task1/HelloWorld.java
@@ -3,6 +3,8 @@ package sk.com.ymca.java.lecture_0.practicke.task1;
 public class HelloWorld {
     public static void main(String[] args) {
 
+        // I'm here
+
         System.out.println("HelloWorld");
         System.out.println("Hello_evrybody_" + "and_my_friends");
     }
