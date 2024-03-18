c6591f792653b43625301542f7bdcdeb05e5efe5
Oleksandr <78863703+verhovcev214265@users.noreply.github.com>
2024-03-18 13:57:16 +0100

    01_Feedback

--- task1/HelloWorld.java
+++ task1/HelloWorld.java
@@ -3,7 +3,7 @@ package sk.com.ymca.java.lecture_0.practicke.task1;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        // I'm here
+        // Here must be comment of the task and you should see the differences between your text and text of the task
 
         System.out.println("HelloWorld");
         System.out.println("Hello_evrybody_" + "and_my_friends");
