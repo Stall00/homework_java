3d6d720a7c576f08edd62ef1dcaf13a9dac33b17
Oleksandr <78863703+verhovcev214265@users.noreply.github.com>
2024-03-18 13:49:07 +0100

    01_Feedback

--- task2/Work2.java
+++ task2/Work2.java
@@ -1,9 +1,18 @@
 package sk.com.ymca.java.lecture_0.practicke.task2;
 
+  // There must be a comment on the task.
+
 public class Work2 {
     public static void main(String[] args) {
-        String messege = "hello_world";
-        String words = "JAVA_FOREVER!!!!!";
+        String messege = "hello_world"; // the text must be "Hello world" and the name of the variable should be "message"
+        //Here must be the action: System.out.println(messege);
+        
+        String words = "JAVA_FOREVER!!!!!";  // the text must be "Java forever "
+        //Here must be the action: System.out.println() and this action must print:
+                                                    /* 
+                                                    Hello world
+                                                    Java forever
+                                                    */  
         String space = " ";
         System.out.println(messege + space + words);
     }
