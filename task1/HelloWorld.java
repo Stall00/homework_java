3d897c323568ca878f58d0bb834e4a43d40051c3
Oleksandr <78863703+verhovcev214265@users.noreply.github.com>
2024-03-18 13:42:47 +0100

    01_feedback

--- task1/HelloWorld.java
+++ task1/HelloWorld.java
@@ -3,7 +3,9 @@ package sk.com.ymca.java.lecture_0.practicke.task1;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("HelloWorld");
-        System.out.println("Hello_evrybody_" + "and_my_friends");
+        // There must be a comment on the task.
+
+        System.out.println("HelloWorld"); // In the task I wrote "Hello world"
+        System.out.println("Hello_evrybody_" + "and_my_friends"); // delete underline in the text.
     }
 }
